393c45e7295d8da9951595df361c7312b9eedfa0
Saurabh Sharma <39699314+saurabhsharma2395@users.noreply.github.com>
2024-03-11 23:30:43 -0400

    NextJS Backend Service

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors({
-    origin: 'http://localhost:3000',
+    origin: 'https://crypto-updated.vercel.app/',
     credentials: true, // This is important for sending cookies over CORS
   }));
 
